48b7b8167597abe990c565be7be2d0ea0b61d71c
Cristopher <cristopher.gaziano@gmail.com>
2023-01-08 17:33:25 +0100

    configure application with dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import "dotenv/config";
+
 import app from "./app";
 
-const port = 3001;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
 
 // Use the techniques you've learnt so far to create your own HTTP server with Express. Your server should:
 
