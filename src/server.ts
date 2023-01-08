eb453284677a2d33de5022408a38b6f2dedee701
Cristopher <cristopher.gaziano@gmail.com>
2023-01-08 16:30:46 +0100

    configure automatic builds and restarts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
-type Name = string;
+import express from "express";
+import "express-async-errors";
 
-const name: Name = "Cristopher";
+const app = express();
 
-throw new Error ("Something went wrong");
+app.get("/", (request, response) => {
+    response.send("Il server è online");
+});
 
-console.log(`Ciao, sono ${name}`);
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+// Use the techniques you've learnt so far to create your own HTTP server with Express. Your server should:
+
+// Automatically recompile and restart when you make changes
+// Have a GET route that sends a JSON response
+// Have an integration test for the GET route
+// Use an environment variable to configure the server port
